65571af30cddc00ef694df226da7d210788a96fc
Chew Hui Feng <tristan.chf@gmail.com>
2021-08-01 16:33:56 +0800

    Updating of BrainTumorSegmentation flask api
    - remove redundant libraries
    - new post route to support different input

--- api/BrainTumorSegmentation.py
+++ api/BrainTumorSegmentation.py
@@ -5,18 +5,15 @@ from flask import jsonify
 from flask import request
 
 import tensorflow as tf
-import tensorflow.keras as keras
-import cv2 as cv
 import numpy as np
-import pandas as pd
-import os
 from PIL import Image
+import cv2 as cv
 import io
 from flask import send_file
 
 app = Flask(__name__)
 
-model = tf.keras.models.load_model('brain_tumor_segmentation_model_v5')
+model = tf.keras.models.load_model('static/model/brain_tumor_segmentation_model_v5')
 
 max_pixel = 11073.25
 
@@ -40,14 +37,78 @@ def generate_mask_color2(mask):
 def predictImage():
     image = np.load(request.files["file"])
 
-    print(image.shape)
-
     #image = cv.resize(np.array(image).astype(np.float64), dsize=(256, 256), interpolation=cv.INTER_AREA)
 
     predicted = model.predict(np.array([image/11073.25]))
     predicted_processed = generate_mask_color2(predicted[0])
 
     img = Image.fromarray(predicted_processed.astype('uint8'))
+    
+    img_obj = io.BytesIO()
+    img.save(img_obj, format="PNG")
+    img_obj.seek(0)
+
+    return send_file(img_obj, mimetype='image/PNG')
+
+@app.route("/predictImageWithResize", methods=["POST"])
+def predictImageWithResize():
+    image = np.load(request.files["file"])
+
+    predicted = model.predict(np.array([image/11073.25]))
+    predicted_processed = generate_mask_color2(predicted[0])
+
+    resized_img = cv.resize(predicted_processed, dsize=(512,512), interpolation=cv.INTER_LINEAR)
+    img = Image.fromarray(resized_img.astype('uint8'))
+
+    img_obj = io.BytesIO()
+    img.save(img_obj, format="PNG")
+    img_obj.seek(0)
+
+    return send_file(img_obj, mimetype='image/PNG')
+
+@app.route("/predictImageWithOverlay", methods=["POST"])
+def predictImageWithOverlay():
+    image = np.load(request.files["file"])
+
+    print(image.shape)
+
+    predicted = model.predict(np.array([image/11073.25]))
+    predicted_processed = generate_mask_color2(predicted[0])
+
+    mask = Image.fromarray(predicted_processed.astype('uint8'))
+    original_image = Image.fromarray(image/11073.25*255)
+
+    mask = mask.convert("RGBA")
+    original_image = original_image.convert("RGBA")
+
+    img = Image.blend(original_image, mask, 0.2)
+
+    img_obj = io.BytesIO()
+    img.save(img_obj, format="PNG")
+    img_obj.seek(0)
+
+    return send_file(img_obj, mimetype='image/PNG')
+
+@app.route("/predictImageWithOverlayResize", methods=["POST"])
+def predictImageWithOverlayResize():
+    image = np.load(request.files["file"])
+
+    print(image.shape)
+
+    predicted = model.predict(np.array([image/11073.25]))
+    predicted_processed = generate_mask_color2(predicted[0])
+
+    mask = cv.resize(predicted_processed, dsize=(512,512), interpolation=cv.INTER_LINEAR)
+    original_image = cv.resize(image, dsize=(512,512), interpolation=cv.INTER_LINEAR)
+
+    mask = Image.fromarray(mask.astype('uint8'))
+    original_image = Image.fromarray(original_image/11073.25*255)
+
+    mask = mask.convert("RGBA")
+    original_image = original_image.convert("RGBA")
+
+    img = Image.blend(original_image, mask, 0.2)
+
     img_obj = io.BytesIO()
     img.save(img_obj, format="PNG")
     img_obj.seek(0)
