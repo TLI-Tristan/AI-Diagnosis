c5848e37904cb23e757867c2943295e96e295c2e
DestinyXC <59255309+DestinyXC@users.noreply.github.com>
2021-08-17 15:53:34 +0800

    Add files via upload

---
+++ client/src/views/Heart3.vue
@@ -0,0 +1,144 @@
+<template>
+<SideNavigation />
+<div class="m">
+<p class="primary-color mt-5 text-2xl font-bold">PATIENT'S NAME: PETER PARKER</p>
+<p class="primary-color mt-5 text-2xl font-bold">PATIENT'S ID No.: 217546</p>
+<p class="primary-color mt-5 text-2xl font-bold">DATE AS OF ECG TEST: 1 JUNE 2021</p>
+<p class="primary-color mt-5 text-2xl font-bold">PATIENT'S DATA:</p>
+</div>
+<div class="table">
+<table>
+<thead>
+  <tr v-for= "item in items" v-bind:key="item.id">
+    <th> Age </th>
+    <td> {{item.age}} </td>
+  </tr>
+  <tr v-for= "item in items" v-bind:key="item.id">
+    <th> Sex </th>
+    <td> {{item.sex}} </td>
+  </tr>
+  <tr v-for= "item in items" v-bind:key="item.id">
+    <th> Chest Pain Type </th>
+    <td> {{item.cp}} </td>
+  </tr>
+  <tr v-for= "item in items" v-bind:key="item.id">
+    <th> Resting Blood Pressure (in mmHg) </th>
+    <td> {{item.trestbps}} </td>
+  </tr>
+  <tr v-for= "item in items" v-bind:key="item.id">
+    <th> Serum Cholesterol (in mg/dl) </th>
+    <td> {{item.chol}} </td>
+  </tr>
+  <tr v-for= "item in items" v-bind:key="item.id">
+    <th> Fasting Blood Sugar > 120 mg/dl </th>
+    <td> {{item.fbs}} </td>
+  </tr>
+  <tr v-for= "item in items" v-bind:key="item.id">
+    <th> Resting ECG Results </th>
+    <td> {{item.restecg}} </td>
+  </tr>
+  <tr v-for= "item in items" v-bind:key="item.id">
+    <th> Maximum Heart-rate achieved </th>
+    <td> {{item.thalach}} </td>
+  </tr>
+  <tr v-for= "item in items" v-bind:key="item.id">
+    <th> Exercise-induced Angina </th>
+    <td> {{item.exang}} </td>
+  </tr>
+  <tr v-for= "item in items" v-bind:key="item.id">
+    <th> ST Depression induced by exercise relative to rest </th>
+    <td> {{item.oldpeak}} </td>
+  </tr>
+  <tr v-for= "item in items" v-bind:key="item.id">
+    <th> Slope of the peak exercise ST segment </th>
+    <td> {{item.slope}} </td>
+  </tr>
+  <tr v-for= "item in items" v-bind:key="item.id">
+    <th> Number of Major Vessels colored by fluorosopy </th>
+    <td> {{item.ca}} </td>
+  </tr>
+  <tr v-for= "item in items" v-bind:key="item.id">
+    <th> Thalassemia </th>
+    <td> {{item.thal}} </td>
+  </tr>
+</thead>
+</table>
+</div>
+</template>
+
+<script>
+//import HelloWorld from "@/components/HelloWorld.vue";
+import SideNavigation from "@/components/SideNavigation.vue";
+
+export default {
+  name: "BrainTumor",
+  components: {
+    SideNavigation,
+  },
+  data(){
+    return{
+      items: [
+        {age: 70, sex: "Male", cp: "Asymptomatic", trestbps: 130, chol: 322, fbs: "False", restecg: "Normal", thalach: 109, exang: "No", oldpeak: 2.4, slope: "Flat", ca: 2, thal: "Normal"} 
+      ]
+    }
+  }
+};
+</script>
+
+<style>
+.column {
+  float: left;
+  width: 25%;
+  padding: 5px;
+}
+
+.row::after {
+  content: "";
+  clear: both;
+  display: table;
+}
+
+div.m{
+  text-align:left;
+  margin-left:50px;
+}
+
+.linebreak {
+  height: 3px;
+  background: black;
+  margin-top: 100px;
+}
+
+.linebreak2 {
+  height: 3px;
+  background: black;
+  margin-top: 50px;
+}
+
+.vlinebreak {
+  border-left: 6px solid black;
+  height: 500px;
+  position: absolute;
+  left: 75%;
+  margin-left: -3px;
+  top: 654px;
+}
+
+div.m2{
+  text-align:left;
+  margin-left:50px;
+}
+
+div.m3{
+  text-align:right;
+  margin-right:450px;
+}
+
+.table{
+  border-collapse: collapse;
+  margin: 25px 0;
+  font-size: 0.9em;
+  min-width: 480px;
+}
+
+</style>
---
+++ client/src/views/HeartReport.vue
@@ -0,0 +1,96 @@
+<template>
+    <SideNavigation />
+  <div class="col-span-8 text-white px-8 pt-6 pb-8 mb-4 h-full justify-between">
+        <div class="w-full h-full flex flex-col">
+          <div>
+            <p class="primary-action-color text-6xl mb-5 font-bold text-left">Patient Report</p>
+          </div>
+
+          <div class="col-span-8 text-white px-8 pt-6 pb-8 mb-4 h-full justify-between">
+            <div class="">
+              <div class="text-left mb-10">
+                <label class="block" for="details"> Details: </label>
+                <textarea class="w-full resize-none border rounded-md background-color primary-color" v-model="details" rows="15" type="text" id="details"></textarea>
+              </div>
+              <div class="text-left mb-10">
+                <label class="block" for="confirmation"> Based on the given data of patient, how likely is it that he will have heart disease?: </label>
+                <textarea class="w-full resize-none border rounded-md background-color primary-color" v-model="confirmation" type="text" id="confirmation"> </textarea>
+              </div>
+              <div class="text-left mb-10">
+                <label class="actions" for="details"> Possible Heart Diseases: </label>
+                <textarea class="w-full resize-none border rounded-md background-color primary-color" v-model="actions" rows="5" type="text" id="actions"></textarea>
+              </div>
+            </div>
+            <div class="text-right">
+              <button class="bg-blue-500 hover:bg-blue-700 text-sm text-white font-bold py-2 px-4 rounded" @click="SubmitDetails">Submit</button>
+            </div>
+          </div>
+
+        </div>
+      </div>
+</template>
+
+<script>
+//import HelloWorld from "@/components/HelloWorld.vue";
+import SideNavigation from "@/components/SideNavigation.vue";
+//import Heart from "@/components/Heart.vue";
+
+export default {
+  name: "Heart",
+  components: {
+      SideNavigation,
+  },
+  data(){
+    return{
+      items: [
+        ]
+    }
+  },
+  methods:{
+    SubmitDetails(){
+      this.$router.push('/thankyou')
+    },
+  }
+};
+</script>
+
+<style>
+table, th, td {
+  border: 1px solid black;
+}
+
+table {
+  border-collapse: collapse;
+    margin: 50px 0;
+    margin-left: 50px;
+    font-size: 0.9em;
+    font-family: sans-serif;
+    min-width: 2510px;
+    box-shadow: 0 0 20px rgba(0, 0, 0, 0.15);
+    background-color: #009879;
+}
+
+thead, tr {
+    color: #ffffff;
+    text-align: left;
+}
+
+th, td {
+  padding: 12px 15px;
+}
+
+tbody tr {
+  border-bottom: 1px solid #dddddd;
+}
+
+
+tbody tr:last-of-type {
+    border-bottom: 2px solid #009879;
+}
+
+.button {
+  transition-duration: 0.4s;
+  background-color:blue;
+}
+
+</style>
